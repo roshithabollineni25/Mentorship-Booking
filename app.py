2f3c833d49498c9f6094ab9701fe39c12f656e47
roshithabollineni25 <roshithabollineni25@gmail.com>
2026-06-03 09:36:07 +0530

    Fixed Mongo URI for Render

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
 from flask import Flask
 from flask_restx import Api
 from flask_cors import CORS
+import os
 
 from config.database import mongo
 
 from routes.auth import auth_ns
 from routes.mentor import mentor_ns
-
 from routes.booking import booking_ns
-
 from routes.review import review_ns
 from routes.analytics import analytics_ns
-
 from routes.availability import availability_ns
-
-
-
 
 app = Flask(__name__)
 
 CORS(app)
 
-# MongoDB Atlas URI
-import os
-
+# MongoDB URI from Render Environment Variables
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+print("Mongo URI Exists:", bool(app.config["MONGO_URI"]))
+
 mongo.init_app(app)
 
 api = Api(
@@ -35,7 +31,6 @@ api = Api(
     doc="/"
 )
 
-# Register Namespaces
 api.add_namespace(auth_ns)
 api.add_namespace(mentor_ns)
 api.add_namespace(booking_ns)
@@ -46,6 +41,6 @@ api.add_namespace(availability_ns)
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=5000,
+        port=int(os.environ.get("PORT", 5000)),
         debug=True
     )
